1f1cec002fc465a6bcde10341ead003bd9ba4112
NeonJacketGuy <linko.dman@gmail.com>
2026-04-23 17:41:16 -0600

    Final animation fixes and final push before work begins on The Hobbit -the game

--- scenes/player/player_animations.gd
+++ scenes/player/player_animations.gd
@@ -1,24 +1,23 @@
 extends Node2D
-#
-#@export var anim_tree: AnimationTree
-#@onready var player: Player = get_owner()
-#
-#var facing = 0
-#
-#func _physics_process(_delta) -> void:
-	#var state = player.current_state
-	#
-	#if player.velocity.x != 0:
-		#facing = player.velocity.x
-	#
-	## setup conditions based on current state
-	#anim_tree.set("parameters/conditions/Falling", state == Player.PlayerStates.FALLING)
-	#anim_tree.set("parameters/conditions/Idle", state == Player.PlayerStates.IDLE)
-	#anim_tree.set("parameters/conditions/Jumping", state == Player.PlayerStates.JUMPING)
-	#anim_tree.set("parameters/conditions/Walking", state == Player.PlayerStates.WALKING)
-#
-	## set blend to player velocity x
-	#anim_tree.set("parameters/Falling/blend_position", facing)
-	#anim_tree.set("parameters/Idle/blend_position", facing)
-	#anim_tree.set("parameters/Jumping/blend_position", facing)
-	#anim_tree.set("parameters/Walking/blend_position", facing)
+
+@export var anim_tree: AnimationTree
+@onready var player: Player = get_owner()
+
+var facing = 0
+
+func _physics_process(delta) -> void:
+	var state = player.current_state
+
+	if player.velocity.x != 0:
+		facing = player.velocity.x
+
+	anim_tree.set("parameters/conditions/Falling", state == Player.PlayerStates.FALLING)
+	anim_tree.set("parameters/conditions/Idle", state == Player.PlayerStates.IDLE)
+	anim_tree.set("parameters/conditions/Jumping", state == Player.PlayerStates.JUMPING)
+	anim_tree.set("parameters/conditions/Walking", state == Player.PlayerStates.WALKING)
+
+
+	anim_tree.set("parameters/Falling/blend_position", facing)
+	anim_tree.set("parameters/Idle/blend_position", facing)
+	anim_tree.set("parameters/Jumping/blend_position", facing)
+	anim_tree.set("parameters/Walking/blend_position", facing)
